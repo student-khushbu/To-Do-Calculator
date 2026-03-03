620d27a82246542b85b3019aa4b9d1a5528dfe7a
student-khushbu <khushbushah7828859207@gmail.com>
2026-03-03 20:41:22 +0530

    javascript code for To Do calculator

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let input = document.getElementById("inputBox");
-let buttons =document.querySelectorAll("button");
+let input = document.getElementById('inputBox');
+let buttons =document.querySelectorAll('button');
+
+let string = "";
 let arr = Array.from(buttons);
-arr.forEach(botton =>{
-    buttons.addEventListener("click" ,(e) =>{
-        if(e.target.innerHTML == "="){
+arr.forEach(button =>{
+    button.addEventListener('click' ,(e) =>{
+        if(e.target.innerHTML == '='){
             string = eval(string);
             input.value = string ;
         }
-        else if(e.target.innerHTML =="AC"){
+        else if(e.target.innerHTML == 'AC'){
             string = "";
             input.value = string ;
         } 
-        else if (e.target.innerHTML == "DEL"){
+        else if (e.target.innerHTML == 'DEL'){
             string = string.substring(0 , string.length-1);
             input.value = string ;
         }
